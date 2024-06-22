99f179ef6b0eaab0d2e1f4a5a3200006dc773b43
James Suderman <jamessuderman@gmail.com>
2024-06-22 14:01:04 -0500

    Navbar transition and footer update

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,3 +1,16 @@
 export default function Hero() {
-  return <div className="hero-container"></div>;
+  return (
+    <div className="hero-container">
+      <div className="hero-cta-container">
+        <p>
+          Marfa pickled pour-over portland. Mumblecore artisan pok pok jean
+          shorts raw denim photo booth poutine succulents fam. Fit bitters
+          edison bulb, franzen humblebrag single-origin coffee mumblecore put a
+          bird on it pabst umami taxidermy. Tumblr cupping fixie swag prism
+          jianbing fanny pack flexitarian cronut Brooklyn.
+        </p>
+        <button>Call to Action</button>
+      </div>
+    </div>
+  );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,28 @@
+import { useState, useEffect } from "react";
 import { FaS } from "react-icons/fa6";
 
 export default function Navbar() {
+  const [scrollYPosition, setScrollYPosition] = useState(0);
+
+  const handleScroll = () => {
+    let newScrollYPosition = window.scrollY;
+    setScrollYPosition(newScrollYPosition);
+
+    if (scrollYPosition <= 1 || newScrollYPosition <= 1) {
+      let navbar = document.getElementById("navbar");
+      navbar.classList.toggle("navbar-scroll");
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
+
   return (
-    <div className="navbar-container">
+    <div id="navbar" className="navbar-container">
       <div className="navbar-item">
         <FaS className="navbar-icon" />
         <span className="navbar-brand-main">Suderman</span>
